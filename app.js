b78ff6b3ebb10dfcba9d6dfa3ecbc295d80ed30d
niloyKGhosh <niloykumar256@gmail.com>
2020-07-28 21:01:09 -0400

    Fixed bug

--- app.js
+++ app.js
@@ -2,12 +2,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 
+const app = express();
+
+// Getting all routers
 const incomesRouter = require("./routes/incomes");
 const expenseRouter = require("./routes/expenses");
-
-const port = process.env.PORT || 3000;
-
-const app = express();
 
 app.use(morgan("tiny"));
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -15,6 +14,8 @@ app.use(bodyParser.json());
 
 app.use(incomesRouter);
 app.use(expenseRouter);
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
 	console.log(`Server is running at port: ${port}`);
