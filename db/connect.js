4474a1d1bc19fdea41c1b482da79f56a73b59512
niloyKGhosh <niloykumar256@gmail.com>
2020-07-28 22:20:01 -0400

    Added income schema

--- db/connect.js
+++ db/connect.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connect = () =>
 	mongoose.connect(
-		"mongodb://localhost:27017",
+		"mongodb://localhost:27017/pocket",
 		{
 			useNewUrlParser: true,
 			useUnifiedTopology: true,
--- models/incomeModel.js
+++ models/incomeModel.js
@@ -2,4 +2,24 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const incomeSchema = new Schema({});
+const incomeSchema = new Schema(
+	{
+		title: {
+			type: String,
+			required: true,
+		},
+		amount: {
+			type: Number,
+			required: true,
+		},
+		dateCreated: {
+			type: String,
+			required: true,
+		},
+	},
+	{ timestamps: true }
+);
+
+const Income = mongoose.model("income", incomeSchema);
+
+module.exports = Income;
