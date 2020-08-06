d483b9b8fde9973d20347489570aa1a201371964
Raed56 <rseraj@usf.edu>
2020-08-05 21:39:19 -0400

    added validations for expen se

--- models/expense.model.js
+++ models/expense.model.js
@@ -3,26 +3,32 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const expenseSchema = new Schema(
-	{
-		title: {
-			type: String,
-			required: true,
-		},
-		amount: {
-			type: Number,
-			required: true,
-		},
-		dateCreated: {
-			type: String,
-			required: true,
-		},
-		expenseId: {
-			type: String,
-			required: true,
-			unique: true,
-		},
-	},
-	{ timestamps: true }
+    {
+        title: {
+            type: String,
+            required: true,
+        },
+        amount: {
+            type: Number,
+            required: true,
+            validate: {
+                validator: function (amount) {
+                    return amount > 0;
+                },
+                message: `Amount must be greater than 0`,
+            },
+        },
+        dateCreated: {
+            type: String,
+            required: true,
+        },
+        expenseId: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+    },
+    { timestamps: true }
 );
 
 const Expense = mongoose.model("expense", expenseSchema);
