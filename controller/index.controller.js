2c9c8ff399033ec447bab83f1c0e29449f274197
Raed56 <rseraj@usf.edu>
2020-08-06 23:03:27 -0400

    added total expense and total income function

--- controller/index.controller.js
+++ controller/index.controller.js
@@ -1,5 +1,15 @@
-module.exports.getIndex = (req, res, next) => {
-  res.json({ meessage: "inside the index controller" });
+const {
+    totalExpense,
+    totalIncome,
+    fetchAllExpenses,
+    fetchAllIncomes,
+} = require("../utils/income-expense.utils");
+
+module.exports.getIndex = async (req, res, next) => {
+    console.log("Total Expense = ", await totalExpense());
+    console.log("Total Income = ", await totalIncome());
+
+    res.json({ meessage: "inside the index controller" });
 };
 
 // TODO: fetch all expenses
--- utils/income-expense.utils.js
+++ utils/income-expense.utils.js
@@ -1,14 +1,33 @@
 const Expense = require("../models/expense.model");
 const Income = require("../models/income.model");
 const fetchAllExpenses = function () {
-	return Expense.find({}).exec();
+    return Expense.find({}).exec();
 };
 
 const fetchAllIncomes = function () {
-	return Income.find({}).exec();
+    return Income.find({}).exec();
+};
+
+const totalIncome = async () => {
+    const allIncomes = await fetchAllIncomes();
+    let sum = 0;
+    for (let income of allIncomes) {
+        sum += income.amount;
+    }
+    return sum;
+};
+const totalExpense = async () => {
+    const allExpenses = await fetchAllExpenses();
+    let sum = 0;
+    for (let expense of allExpenses) {
+        sum += expense.amount;
+    }
+    return sum;
 };
 
 module.exports = {
-	fetchAllExpenses,
-	fetchAllIncomes,
+    totalExpense,
+    totalIncome,
+    fetchAllExpenses,
+    fetchAllIncomes,
 };
