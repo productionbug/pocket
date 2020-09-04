d6f6f68ac31932d5bff71a06baafe7b463ee5f06
niloyKGhosh <niloykumar256@gmail.com>
2020-09-04 07:45:47 -0400

    populated the table

--- controller/index.controller.js
+++ controller/index.controller.js
@@ -5,15 +5,15 @@ module.exports.getIndex = async (req, res, next) => {
 		const totalIncome = await incomeExpenseUtil.totalIncome();
 		const totalExpense = await incomeExpenseUtil.totalExpense();
 		const currentBalance = await incomeExpenseUtil.currentBalance();
-		console.log(
-			`totalInc = ${totalIncome}, totalExp = ${totalExpense}, currBalance = ${currentBalance}`
-		);
+		const allExpenses = await incomeExpenseUtil.fetchAllExpenses();
+		const allIncomes = await incomeExpenseUtil.fetchAllIncomes();
 
-		// res.json({ message: "inside the index controller" });
 		res.render("../views/index.ejs", {
 			totalExpense: totalExpense,
 			totalIncome: totalIncome,
 			currentBalance: currentBalance,
+			allIncomes: allIncomes,
+			allExpenses: allExpenses,
 		});
 	} catch (e) {
 		// ! dont send the error to the controller
